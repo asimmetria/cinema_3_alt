2efa28f6b3387363f35aa0267fe9a512db8b802f
Vladislav Trotsky <87899580+vamp666@users.noreply.github.com>
2023-03-17 03:06:42 +0400

    feat: <Инициализация TestDataInitializer> [issue #6]

--- src/main/java/com/kata/cinema/base/repository/UserRepository.java
+++ src/main/java/com/kata/cinema/base/repository/UserRepository.java
@@ -2,7 +2,17 @@ package com.kata.cinema.base.repository;
 
 import com.kata.cinema.base.models.entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    @Query("SELECT new com.kata.cinema.base.models.dto.response.UserResponseDto(" +
+            "us.id," +
+            "us.email," +
+            "us.name," +
+            "us.lastName," +
+            "us.password," +
+            "us.birthday) FROM User us WHERE us.id = :userId")
+    UserResponseDto getUserById(@Param("userId") Long userId);
+
 }
