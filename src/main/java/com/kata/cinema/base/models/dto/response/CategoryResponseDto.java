e7df16a700705609d678c24f3df3eb8e276ff0ea
Vladislav Trotsky <87899580+vamp666@users.noreply.github.com>
2023-03-23 21:58:57 +0400

    fix: <Категории коллекций> [Добавлены ResponseEntities] [17] r with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/kata/cinema/base/models/dto/response/CategoryResponseDto.java
+++ src/main/java/com/kata/cinema/base/models/dto/response/CategoryResponseDto.java
@@ -1,7 +1,12 @@
 package com.kata.cinema.base.models.dto.response;
 
 import com.kata.cinema.base.models.entitys.CollectionCategories;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponseDto {
 
     private Long id;
@@ -10,21 +15,5 @@ public class CategoryResponseDto {
     public CategoryResponseDto(CollectionCategories category) {
         this.id = category.getId();
         this.name = category.getName();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/main/java/com/kata/cinema/base/webapp/controller/admin/AdminCollectionCategoryRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controller/admin/AdminCollectionCategoryRestController.java
@@ -4,7 +4,16 @@ import com.kata.cinema.base.models.dto.response.CategoryResponseDto;
 import com.kata.cinema.base.models.entitys.CollectionCategories;
 import com.kata.cinema.base.service.entity.CollectionCategoryService;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,34 +29,38 @@ public class AdminCollectionCategoryRestController {
     }
 
     @GetMapping
-    public List<CategoryResponseDto> getCategories() {
+    public ResponseEntity<List<CategoryResponseDto>> getCategories() {
         List<CollectionCategories> categories = categoryService.getAllCategories();
-        return categories.stream()
+        List<CategoryResponseDto> categoryResponseDtos = categories.stream()
                 .map(CategoryResponseDto::new)
                 .collect(Collectors.toList());
+        return ResponseEntity.ok(categoryResponseDtos);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         CollectionCategories category = categoryService.getCategoryById(id);
         if (category == null) {
             throw new EntityNotFoundException("Category with id " + id + " not found");
         }
         categoryService.deleteCategory(category);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public void updateCategory(@PathVariable Long id, @RequestParam String name) {
+    public ResponseEntity<Void> updateCategory(@PathVariable Long id, @RequestParam String name) {
         CollectionCategories category = categoryService.getCategoryById(id);
         if (category == null) {
             throw new EntityNotFoundException("Category with id " + id + " not found");
         }
         category.setName(name);
         categoryService.updateCategory(category);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
-    public void createCategory(@RequestParam String name) {
+    public ResponseEntity<Void> createCategory(@RequestParam String name) {
         categoryService.createCategory(name);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
