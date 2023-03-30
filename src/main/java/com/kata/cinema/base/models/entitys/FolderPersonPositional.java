f4710e615066a3f2f63d2d0ad8068650e003f1e4
Natalya Botyasova <mrrnyash@gmail.com>
2023-03-30 15:50:15 +0800

    refactor: relationships between folders and persons/movies
    removed ManyToMany relationship between Person and FolderPerson entities, added FolderMoviePositional and FolderPersonPositional entities
    
    issue #17

---
+++ src/main/java/com/kata/cinema/base/models/entitys/FolderPersonPositional.java
@@ -0,0 +1,38 @@
+package com.kata.cinema.base.models.entitys;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "folders_persons_positional")
+public class FolderPersonPositional {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "person", nullable = false)
+    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "folder", nullable = false)
+    private FolderPerson folder;
+
+    @Column(name = "positional")
+    private Integer positional;
+
+}
