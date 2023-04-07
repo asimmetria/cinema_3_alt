975591903e6bd799064c12f482134171d28c5eae
Legion-fire <Legion-fire@yandex.ru>
2023-04-07 16:08:20 +0300

    <fix> [service]: <Работа с комментариями> [убрал запрос из цикла, убрал comment] [issue #"11"]

--- src/main/java/com/kata/cinema/base/service/dto/impl/CommentDtoServiceImpl.java
+++ src/main/java/com/kata/cinema/base/service/dto/impl/CommentDtoServiceImpl.java
@@ -2,14 +2,12 @@ package com.kata.cinema.base.service.dto.impl;
 
 import com.kata.cinema.base.models.dto.response.UserCommentResponseDto;
 import com.kata.cinema.base.models.dto.response.UserNameResponseDto;
-import com.kata.cinema.base.models.entitys.Comment;
 import com.kata.cinema.base.repository.CommentRepository;
 import com.kata.cinema.base.service.dto.CommentDtoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,15 +23,16 @@ public class CommentDtoServiceImpl implements CommentDtoService {
     @Override
     public List<UserCommentResponseDto> listWellDoneUserCommentResponseDto(Long mediaId) {
         List<UserCommentResponseDto> result = listRawUserCommentResponseDto(mediaId);
-        for (Long id : listRawUserCommentResponseDto(mediaId).stream().map(UserCommentResponseDto::getId).toList()){
-            Comment comment = new Comment();
-            Optional<Comment> optional = commentRepository.findById(id);
-            if(optional.isPresent()) comment = optional.get();
-            result.get(Math.toIntExact(id)).setId(comment.getId());
-            result.get(Math.toIntExact(id)).setUser(getUserDtoByCommentIds(mediaId).get(comment.getUser().getId().intValue()));
-            result.get(Math.toIntExact(id)).setParentId(comment.getParentComment().getId());
-            result.get(Math.toIntExact(id)).setLevel(comment.getLevel());
-            result.get(Math.toIntExact(id)).setMessage(comment.getMessage());
+        List<Long> ids = listRawUserCommentResponseDto(mediaId).stream().map(UserCommentResponseDto::getId).toList();
+        List<Long> parentIds = listRawUserCommentResponseDto(mediaId).stream().map(UserCommentResponseDto::getParentId).toList();
+        List<Integer> levels = listRawUserCommentResponseDto(mediaId).stream().map(UserCommentResponseDto::getLevel).toList();
+        List<String> messages = listRawUserCommentResponseDto(mediaId).stream().map(UserCommentResponseDto::getMessage).toList();
+        for (Long id : ids){
+            result.get(Math.toIntExact(id)).setId(id);
+            result.get(Math.toIntExact(id)).setUser(getUserDtoByCommentIds(mediaId).get(Math.toIntExact(id)));
+            result.get(Math.toIntExact(id)).setParentId(parentIds.get(Math.toIntExact(id)));
+            result.get(Math.toIntExact(id)).setLevel(levels.get(Math.toIntExact(id)));
+            result.get(Math.toIntExact(id)).setMessage(messages.get(Math.toIntExact(id)));
         }
         return result;
     }
