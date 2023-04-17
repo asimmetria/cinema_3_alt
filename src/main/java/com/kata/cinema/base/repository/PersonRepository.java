beb8ff4ed96f58a7a387c246d185fe4c1e0c5bce
Natalya Botyasova <mrrnyash@gmail.com>
2023-04-17 20:10:58 +0800

    fix [Repository, Converter]: removed unused classes
    issue #21

--- src/main/java/com/kata/cinema/base/repository/PersonRepository.java
+++ src/main/java/com/kata/cinema/base/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.kata.cinema.base.repository;
 
 import com.kata.cinema.base.models.entitys.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
