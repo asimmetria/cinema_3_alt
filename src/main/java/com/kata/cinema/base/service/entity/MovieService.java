79931de67c217a956563ad57a82d5efb075aa364
Wiack <viiakko@gmail.com>
2023-05-05 09:14:25 +0300

    <refactor>: TODO в MovieServiceFacadeImpl
    создать отдельный бизнес сервис MovieViewService.
    В  MovieServiceFacadeImpl использовать класс валидации.
    Из MovieServiceFacadeImpl вынести логику  в MovieServiceImpl и новосозданный MovieViewServiceImpl
    #32 task

--- src/main/java/com/kata/cinema/base/service/entity/MovieService.java
+++ src/main/java/com/kata/cinema/base/service/entity/MovieService.java
@@ -1,11 +1,12 @@
 package com.kata.cinema.base.service.entity;
 
+import com.kata.cinema.base.models.dto.request.MovieRequestDto;
 import com.kata.cinema.base.models.entitys.Movie;
 
 public interface MovieService {
     Movie getMovie(Long id);
 
-    void save(Movie movie);
+    void save(Long id, MovieRequestDto movieDto);
 
     void deleteById(Long id);
 
