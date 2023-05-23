8974d13d240ec4302db9d29ae928e1cca56e2d38
DrKringe <Tuork@yandex.ru>
2023-05-23 21:00:55 +0300

    сделал2

--- src/main/java/com/kata/cinema/base/repository/PersonRepository.java
+++ src/main/java/com/kata/cinema/base/repository/PersonRepository.java
@@ -12,15 +12,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query("SELECT NEW com.kata.cinema.base.models.dto.response.PersonViewResponseDto(p.id, p.previewUrl, CONCAT(p.firstName, ' ', p.lastName), CONCAT(p.originalFirstName, ' ', p.originalLastName), p.height, " +
-            "(SELECT COUNT(m) FROM Movie m JOIN m.casts c WHERE c.person.id = p.id), " +
-            "(SELECT NEW com.kata.cinema.base.models.dto.response.GenreResponseDto(g.genreName) FROM Movie m JOIN m.casts c JOIN m.genres g WHERE c.person.id = p.id GROUP BY g.genreName ORDER BY COUNT(g) DESC LIMIT 3), " +
-            "(SELECT NEW com.kata.cinema.base.models.dto.response.ProfessionResponseDto(c.profession.professionName) FROM Cast c WHERE c.person.id = p.id), " +
-            "c.profession.professionName) " +
+            "(SELECT COUNT(m) FROM Movie m JOIN m.cast c WHERE c.person.id = p.id), " +
+            "(SELECT NEW com.kata.cinema.base.models.dto.response.GenreResponseDto(g.name) FROM Movie m JOIN m.cast c JOIN m.genre g WHERE c.person.id = p.id GROUP BY g.name ORDER BY COUNT(g) DESC LIMIT 3), " +
+            "(SELECT NEW com.kata.cinema.base.models.dto.response.ProfessionResponseDto(c.profession.name) FROM Cast c WHERE c.person.id = p.id), " +
+            "c.profession.name) " +
             "FROM Person p " +
             "LEFT JOIN p.casts c " +
             "LEFT JOIN c.movie m " +
             "WHERE p.id = :personId " +
-            "GROUP BY p.id, c.profession.professionName")
+            "GROUP BY p.id, c.profession.name")
     PersonViewResponseDto getPersonViewById(@Param("personId") Long personId);
 
     Person getPersonById(Long id);
