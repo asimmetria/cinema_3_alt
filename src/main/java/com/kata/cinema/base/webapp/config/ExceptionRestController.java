1192ffbc93382dd9252f59d6e155f6856c9dabed
zaborskiy <zaborskiy_ai@mail.ru>
2023-06-05 17:05:26 +0300

    refactors: всего и вся

--- src/main/java/com/kata/cinema/base/webapp/config/ExceptionRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/config/ExceptionRestController.java
@@ -13,18 +13,18 @@ import java.util.Date;
 @RestControllerAdvice
 public class ExceptionRestController {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> globalException(RuntimeException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
-    }
-
-    @ExceptionHandler(NotFoundEntityException.class)
-    public ResponseEntity<ErrorResponse> notFoundEntityExceptional(NotFoundEntityException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
-    }
-
-    @ExceptionHandler(NoSuchConnectionException.class)
-    public ResponseEntity<ErrorResponse> deleteException(NoSuchConnectionException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
-    }
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<ErrorResponse> globalException(RuntimeException exception) {
+//        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
+//    }
+//
+//    @ExceptionHandler(NotFoundEntityException.class)
+//    public ResponseEntity<ErrorResponse> notFoundEntityExceptional(NotFoundEntityException exception) {
+//        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
+//    }
+//
+//    @ExceptionHandler(NoSuchConnectionException.class)
+//    public ResponseEntity<ErrorResponse> deleteException(NoSuchConnectionException exception) {
+//        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), new Date()).getHttpStatus());
+//    }
 }
