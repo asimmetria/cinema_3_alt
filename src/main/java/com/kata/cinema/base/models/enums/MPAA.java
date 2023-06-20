98075bc0e852d175b991679d245eb81da4bfb963
Emile <emabish42@gmail.com>
2023-06-20 10:41:15 +0200

    <feat>: “Initialize Movie, Genre, and Collection entities” [issue #29]

--- src/main/java/com/kata/cinema/base/models/enums/MPAA.java
+++ src/main/java/com/kata/cinema/base/models/enums/MPAA.java
@@ -1,8 +1,18 @@
 package com.kata.cinema.base.models.enums;
 
+import java.util.List;
+import java.util.Random;
+
 public enum MPAA {
     GENERAL_AUDIENCES,
     PARENTAL_GUIDANCE_SUGGESTED,
     PARENTS_STRONGLY_CAUTIONED,
-    NO_ONE_SEVENTEEN_AND_UNDER_ADMITTED
+    NO_ONE_SEVENTEEN_AND_UNDER_ADMITTED;
+
+    private static final List<MPAA> VALUES =
+            List.of(values());
+
+    public static MPAA randomMPAA() {
+        return VALUES.get(new Random().nextInt(0, VALUES.size()));
+    }
 }
